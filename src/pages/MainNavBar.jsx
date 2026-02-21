dc681a15accf938feae8bfa679b6d536f90fa256
Ram <2500040211@kluniversity.in>
2026-02-21 10:57:38 +0530

    modified MainNavBar.jsx

--- src/pages/MainNavBar.jsx
+++ src/pages/MainNavBar.jsx
@@ -15,7 +15,7 @@ const MainNavBar = () => {
       <div className="navbar-container">
         <div className="navbar-logo">
           <Link to="/" className="logo-link">
-            KL Event Management
+            KL Event Management System 2500040211
           </Link>
         </div>
         <ul className="nav-menu">
