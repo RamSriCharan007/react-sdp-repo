675e332e36002096e23c9bcb0722af2f1ac19d62
Ram <2500040211@kluniversity.in>
2026-02-23 20:46:43 +0530

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import MainNavBar from './pages/MainNavBar';
 import AdminNavBar from './admin/AdminNavBar';
-import ManagerNavBar from './manager/ManagerNavBar';
+import ManagerNavBar from './Manager/ManagerNavBar';
 import CustomerNavBar from './customer/CustomerNavBar';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
   const [isManager, setIsManager] = useState(false);
   const [isCustomer, setIsCustomer] = useState(false);
 
+  useEffect(() => {
     // Check sessionStorage for user role
     const adminStatus = sessionStorage.getItem('isAdmin') === 'true';
     const managerStatus = sessionStorage.getItem('isManager') === 'true';
